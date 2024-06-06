ff74c040c6d7344ae902fcc6a6841e8636e1ac3a
RohitMangale <119059546+RohitMangale@users.noreply.github.com>
2024-06-07 01:28:21 +0530

    connection.3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:4000/api";
+export const BASE_URL = "https://doctor-appointment-sigma-blue.vercel.app/api";
 export const token = localStorage.getItem('token');
